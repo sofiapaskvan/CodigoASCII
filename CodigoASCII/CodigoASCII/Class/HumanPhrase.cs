11efdc38539a858f3ec8ec11d381363e918a5751
Sofia Paskvan <sofiapc88@gmail.com>
2023-08-06 23:01:55 +0200

    programa

---
+++ CodigoASCII/CodigoASCII/Class/HumanPhrase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoASCII.Class
+{
+    public class HumanPhrase
+    {
+        public int Code { get; set; }
+        public string Phrase { get; set; }
+
+        public HumanPhrase(int code, string phrase)
+        {
+            Code = code;
+            Phrase = phrase;
+        }
+    }
+}
---
+++ CodigoASCII/CodigoASCII/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using CodigoASCII.Class;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Ruta del archivo .json
+        string fileName = "frases.json";
+        string jsonFolderPath = "JSON"; 
+        string filePath = Path.Combine(jsonFolderPath, fileName);
+
+        string jsonPhrases = File.ReadAllText(filePath);
+
+        // Deserializa las frases  a una lista
+        List<HumanPhrase> phrases = JsonSerializer.Deserialize<List<HumanPhrase>>(jsonPhrases);
+
+        foreach (var phrase in phrases)
+        {
+            string humanPhrase = TranslateCodeToHumanPhrase(phrase.Code);
+            Console.WriteLine($"Código: {phrase.Code}, Frase traducida: {humanPhrase}");
+        }
+
+        Console.ReadLine(); 
+    }
+
+    public static string TranslateCodeToHumanPhrase(long code)
+    {
+        byte[] bytes = BitConverter.GetBytes(code);
+        string result = Encoding.ASCII.GetString(bytes);
+        return result;
+    }
+
+}
